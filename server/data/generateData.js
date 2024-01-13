1de13b2c9a34aba0cdec168442d06c51f7f4d324
AndreeaBak <bakandreea01@gmail.com>
2024-01-13 23:57:18 +0200

    Updated project

--- server/data/generateData.js
+++ server/data/generateData.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const Chance = require('chance');
-const serviceAccount = require('./project-planner-e362b-firebase-adminsdk-cinue-d354a843b0.json'); 
+const serviceAccount = require('../project-planner-e362b-firebase-adminsdk-cinue-d354a843b0.json'); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -37,17 +37,32 @@ const generateTeamMembers = (nrMembrii) => {
   return teamMembers;
 };
 
+//Generare useri
+const generateUsers = (nrUtilizatori) => {
+  const users = [];
+  for (let i = 0; i < nrUtilizatori; i++) {
+    const user = {
+      email: chance.email(),
+      password: 'parolaSecreta',
+    };
+    users.push(user);
+  }
+  return users;
+};
+
 //Adauga date in Firestore
 const addData = async () => {
   const nrProiecte = 10; 
   const nrMembrii = 50; 
+  const nrUtilizatori = 5;
 
   const projects = generateProjects(nrProiecte);
   const teamMembers = generateTeamMembers(nrMembrii);
+  const users = generateUsers(nrUtilizatori);
 
-  // Adăugarea datelor în Firestore
   await db.collection('projects').doc('projects-demo').set({ projects });
   await db.collection('team').doc('team-demo').set({ teamMembers });
+  await db.collection('users').doc('users-demo').set({ users });
 
   console.log('Data generated and added successfully.');
 };
--- server/index.js
+++ server/index.js
@@ -3,46 +3,35 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 
+const port = 3000;
+
 const serviceAccount = require('./project-planner-e362b-firebase-adminsdk-cinue-d354a843b0.json'); 
 
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+console.log('Firebase initialized successfully!');
+
+const projectsRoutes = require('./routes/projects.js');
+const teamMembersRoutes = require('./routes/teamMembers.js');
+const usersRoutes = require('./routes/users.js');
+
+app.use(cors());
+app.use(bodyParser.json());
+
 const db = admin.firestore();
 
 //Routes
-app.get('api/projects', async (req, res) => {
-  const projects = []
-  res.json(projects)
-})
-
-app.get('api/team-members', async (req, res) => {
-  const projects = []
-  res.json(projects)
-})
-
-app.post('api/projects', async(req,res)=> {
-  res.json({message:'Project added successfully'})
-})
-
-app.put('api/projects/:id', async(req,res)=> {
-  res.json({message:'Project added successfully'})
-})
-
-app.delete('api/projects/:id', async(req,res)=> {
-  res.json({message:'Project added successfully'})
-})
+app.use('/api/users', usersRoutes);
+app.use('/api/projects', projectsRoutes);
+app.use('/api/teamMembers', teamMembersRoutes);
 
 //Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listen on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
 });
 
 
